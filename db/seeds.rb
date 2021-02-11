47ce39b033fb51204600d2728cf97af6480df4b2
Manon Garnier <manon.garnier.lehy@gmail.com>
2021-02-11 15:48:17 +0100

    mister cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'net/http'
+require 'json'
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+drinks = JSON.parse(response)
+puts 'Creating ingredients...'
+drinks['drinks'].each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+  puts "Created #{ingredient['strIngredient1']}"
+end
+
+puts 'Creating cocktails...'
+5.times do
+  url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  cocktail = JSON.parse(response)
+  Cocktail.create(name: cocktail['drinks'][0]['strDrink'])
+  puts "Created #{cocktail['drinks'][0]['strDrink']}"
+end
